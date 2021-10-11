73ea0cffcb3c9e44cb6970c916d56e64cfa8dec2
kimyu0218 <serena35@naver.com>
2021-10-11 17:44:28 +0900

    [이분 탐색] 10월 8일

--- 10월 8일 - 이분 탐색/3079.cpp
+++ 10월 8일 - 이분 탐색/3079.cpp
@@ -5,17 +5,13 @@ using namespace std;
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> t; // (first : 심사대 비는 시간 / second : 소요 시간)
 
 int minTime(int m) { // 최소 심사 시간 반환
-    int sec = 0, pass = 0;
-    while(true) {
-        sec++;
-        while(!t.empty()) {
-            if(t.top().first == sec) { // 심사대가 비어있는 경우
-                pass++;
-                if(pass == m) return sec; // 모두 통과한 경우 시간 반환
-                t.push({sec + t.top().second, t.top().second}); t.pop();
-            }
-            else break; // 심사대가 비어있지 않은 경우
-        }
+    int sec, pass = 0;
+    while(!t.empty()) {
+        pass++;
+        sec = t.top().first;
+        if (pass == m) return sec; // 모두 통과한 경우 시간 반환
+        t.push({sec + t.top().second, t.top().second});
+        t.pop();
     }
 }
 
