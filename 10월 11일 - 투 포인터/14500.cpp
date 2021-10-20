07a59607faff2ce0b0a1d94fc73eed3f6caaa62c
kimyu0218 <serena35@naver.com>
2021-10-20 23:10:20 +0900

    [투 포인터] 10월 11일 - Update

--- 10월 11일 - 투 포인터/14500.cpp
+++ 10월 11일 - 투 포인터/14500.cpp
@@ -2,16 +2,51 @@
 #include <vector>
 
 using namespace std;
-vector<vector<int>> t;
+
+int n, m;
+int max_num = 0;
+vector<vector<int>> t, visited;
+
+void dfs(int row, int col, int sum, int len) {
+    if(len == 4) { // 정사각형 4개 이어붙인 경우
+        max_num = max(max_num, sum);
+        return;
+    }
+
+    int dx[4] = {0, 0, -1, 1}, dy[4] = {-1, 1, 0, 0}; // 상 하 좌 우
+    for(int i = 0; i < 4; i++) {
+        int new_row = row + dx[i];
+        int new_col = col + dy[i];
+
+        if(new_row < 0 || new_row >= n || new_col < 0 || new_col >= m) continue; // 경계 넘어가는 경우
+        if(visited[new_row][new_col]) continue; // 이미 방문한 경우
+
+        visited[new_row][new_col] = true;
+        dfs(new_row, new_col, sum + t[new_row][new_col], len+1);
+        visited[new_row][new_col] = false;
+    }
+}
+
+int findMax() {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            visited[i][j] = true;
+            dfs(i, j, t[i][j], 1);
+            visited[i][j] = false;
+        }
+    }
+    return max_num;
+}
 
 int main() {
-    int n, m;
     cin >> n >> m;
 
     t.assign(n, vector<int> (m, 0));
+    visited.assign(n, vector<int> (m, false));
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++)
             cin >> t[i][j];
     }
+    cout << findMax();
     return 0;
 }
