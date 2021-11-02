41743e0882994fc40ca33e2446e566a7e6317de2
kimyu0218 <serena35@naver.com>
2021-11-02 18:30:40 +0900

    [DFS와 BFS] 10월 15일 - Update

--- 10월 15일 - DFS와 BFS/4963.cpp
+++ 10월 15일 - DFS와 BFS/4963.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
@@ -10,30 +9,18 @@ int w, h;
 int dx[8] = {0, 0, -1, 1, -1, -1, 1, 1}, dy[8] = {-1, 1, 0, 0, -1, 1, -1, 1};
 
 vector<vector<int>> i_map;
-vector<vector<bool>> visited;
 
 void dfs(int row, int col) {
-    visited[row][col] = true;
+    i_map[row][col] = 0; // 방문 체크
 
     for(int i = 0; i < 8; i++) {
         int new_row = row + dy[i];
         int new_col = col + dx[i];
         
         // 경계 조건을 만족하고 방문하지 않은 땅인 경우
-        if(new_row >= 0 && new_row < h && new_col >= 0 && new_col < w && i_map[new_row][new_col] && !visited[new_row][new_col])
+        if(new_row >= 0 && new_row < h && new_col >= 0 && new_col < w && i_map[new_row][new_col])
             dfs(new_row, new_col);
     }
-}
-
-int countIsland(queue<cor> q) {
-    int cnt = 0;
-
-    while(!q.empty()) {
-        if(visited[q.front().first][q.front().second]) { q.pop(); continue; } // 이미 방문한 좌표인 경우
-        dfs(q.front().first, q.front().second);
-        cnt++;
-    }
-    return cnt;
 }
 
 int main() {
@@ -42,16 +29,19 @@ int main() {
         cin >> w >> h;
         if(!w && !h) break;
 
-        queue<cor> q;
         i_map.assign(h, vector<int>(w, 0));
-        visited.assign(h, vector<bool>(w, false));
+
+        for(int i = 0; i < h; i++) // 지도 입력
+            for(int j = 0; j < w; j++)
+                cin >> i_map[i][j];
+
+        int cnt = 0;
         for(int i = 0; i < h; i++) {
             for(int j = 0; j < w; j++) {
-                cin >> i_map[i][j];
-                if(i_map[i][j]) q.push({i, j}); // 땅 push
+                if(i_map[i][j]) { cnt++; dfs(i, j); } // 땅
             }
         }
-        cout << countIsland(q) << '\n';
+        cout << cnt << '\n';
     }
 
     return 0;
